a11f9e772cdf5c6101fef8227546e0ab14f57f1c
Johnny8Quid <johnny8bargain@gmail.com>
2022-12-07 10:09:14 +0100

    push

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict'
+function removeClass(e) {
+
+    if (e.propertyName != 'transform') return;
+    e.target.classList.remove('playing');
+}
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"`)
+    if (!audio) return;
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
+
+
+const keys = document.querySelectorAll('.key');
+console.log(keys)
+
+keys.forEach(key => key.addEventListener('transitionend', removeClass));
+window.addEventListener('keydown', playSound);
